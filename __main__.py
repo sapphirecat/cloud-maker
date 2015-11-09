98040f83563b7c3be7fafa67fc5666867dd1b6b4
Sapphire Cat <devel@sapphirepaw.org>
2015-11-08 19:54:58 -0500

    Apply encoding to toplevel files.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+# vim: fileencoding=utf-8
 from __future__ import absolute_import, print_function, unicode_literals
 from pyversion import check as version_check
 import runpy
--- pyversion.py
+++ pyversion.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# vim: fileencoding=utf-8
 from __future__ import print_function, absolute_import, unicode_literals
 import sys
 
