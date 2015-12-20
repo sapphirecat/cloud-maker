2014a071e18750d38d73886e95e08641fa07ccec
Sapphire Cat <devel@sapphirepaw.org>
2015-12-20 14:09:53 -0500

    0.8.3 release.
    * Bump Py3 version to 3.3.4+
    * Add `INI_DIR` variable.

--- cloud_maker/__init__.py
+++ cloud_maker/__init__.py
@@ -1,7 +1,7 @@
 # vim: fileencoding=utf-8
 from __future__ import print_function, absolute_import, unicode_literals
 
-VERSION_INFO = (0,8,2)
+VERSION_INFO = (0,8,3)
 VERSION = '.'.join(str(x) for x in VERSION_INFO)
 
 def version_check ():
