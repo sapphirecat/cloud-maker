0577badfe32b12ab6629cdc3225289b264111102
Sapphire Cat <devel@sapphirepaw.org>
2016-01-24 17:49:10 -0500

    0.8.4 release.

--- cloud_maker/__init__.py
+++ cloud_maker/__init__.py
@@ -1,5 +1,5 @@
 # vim: fileencoding=utf-8
 from __future__ import print_function, absolute_import, unicode_literals
 
-VERSION_INFO = (0,8,3)
+VERSION_INFO = (0,8,4)
 VERSION = '.'.join(str(x) for x in VERSION_INFO)
